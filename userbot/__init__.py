f166063ca58c758f8bfa9c9743442dcf63bce690
Pavua <pavelr7@gmail.com>
2025-06-11 04:19:45 +0200

    Add plugin management commands

---
+++ userbot/__init__.py
@@ -0,0 +1,15 @@
+"""Simple Telegram userbot package."""
+__all__ = [
+    "main",
+    "fine_tune_from_history",
+    "run_ollama_fine_tune",
+    "dataset_from_history",
+    "load_config",
+    "save_config",
+    "start_api",
+]
+
+from .fine_tune import fine_tune_from_history, dataset_from_history, run_ollama_fine_tune
+from .llm import load_config, save_config
+from .api import start_api
+__version__ = '0.1.0'
